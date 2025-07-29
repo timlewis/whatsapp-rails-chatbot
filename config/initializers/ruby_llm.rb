57b89ad7a1c57b1f90a3c96534cbd923cf29d892
Timothy Lewis <tim@timlewis.name>
2025-07-29 16:44:06 +0200

    Update RubyLLM configuration to set default model and refresh models

--- config/initializers/ruby_llm.rb
+++ config/initializers/ruby_llm.rb
@@ -4,6 +4,8 @@ RubyLLM.configure do |config|
   config.openai_api_key = ENV.fetch("OPENAI_API_KEY", nil)
   config.anthropic_api_key = ENV["ANTHROPIC_API_KEY"]
   config.gemini_api_key = ENV["GEMINI_API_KEY"]
+  config.default_model = 'gemini-2.0-flash'
 end
 
+RubyLLM.models.refresh!
 AVAILABLE_MODEL_IDS = RubyLLM.models.all.map(&:id).freeze
